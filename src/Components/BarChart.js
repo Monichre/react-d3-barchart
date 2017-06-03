2243038a897f59d02731500d15be9c0684d58f00
Liam Ellis <liamhellis@gmail.com>
2017-06-03 01:32:33 -0400

    scaling the barchart dynamically

--- src/Components/BarChart.js
+++ src/Components/BarChart.js
@@ -8,8 +8,26 @@ class BarChart extends Component {
     constructor(props){
         super(props);
         this.state = {
-            data: props.data
+            data: props.data,
+            yScale: '',
+            xScale: ''
         }
+    }
+
+    setYScale(){
+        let y = d3.scaleLinear()
+            .domain([0, d3.max(this.state.data)])
+            .range([0, this.props.height]);
+
+        this.setState({yScale: y});
+    }
+
+    setXScale(){
+        let x = d3.scaleBand()
+            .domain(d3.range(0, this.state.data.length))
+            .range([0, this.props.width]);
+
+        this.setState({xScale: x});
     }
 
     static defaultProps = {
@@ -20,6 +38,13 @@ class BarChart extends Component {
         barWidth: 40,
         barOffset: 5
     }
+
+    // Life Cycle Methods
+    componentWillMount(){
+        this.setYScale();
+        this.setXScale();
+    }
+
     render() {
         const chart = ReactFauxDOM.createElement('div');
 
@@ -33,13 +58,14 @@ class BarChart extends Component {
                 .style('fill', this.props.barColor)
                 .attr('width', this.props. barWidth)
                 .attr('height', (d) => {
-                    return d; //This should return the data from the parent state
+                    return this.state.yScale(d); //This should return the data from the parent state
                 })
                 .attr('x', (d, i) => {
-                    return i * (this.props.barWidth + this.props.barOffset)
+                    // return i * (this.props.barWidth + this.props.barOffset)
+                    return this.state.xScale(i);
                 })
                 .attr('y', (d) => {
-                    return this.props.height -d;
+                    return this.props.height - this.state.yScale(d);
                 })
 
         return chart.toReact();
